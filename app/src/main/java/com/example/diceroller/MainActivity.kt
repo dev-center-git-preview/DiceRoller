4ff95d56f2f3f5d53c13d848253cdb445149d3e5
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-02-22 21:46:33 +0200

    Add "Dice" image view and its roll logic.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var resultTextView: TextView
+    private lateinit var diceImage: ImageView
     private lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultTextView = findViewById(R.id.result_textView)
+        diceImage = findViewById(R.id.dice_image)
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -22,6 +21,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        resultTextView.text = "${Random().nextInt(6) + 1}"
+        var randomNumber = Random().nextInt(6) + 1
+        var drawableResourceId = when(randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResourceId)
     }
 }
