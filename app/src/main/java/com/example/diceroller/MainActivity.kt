4b142fc1b5aa027d41d964af005afd6deb45f794
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-02-22 14:25:50 +0200

    Created reference to the "result_textView" on MainActivity

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var resultTextView: TextView
     private lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultTextView = findViewById(R.id.result_textView)
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
         }
