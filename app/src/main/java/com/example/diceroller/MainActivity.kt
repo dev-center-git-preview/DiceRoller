1de46792283b134e0bf10bcc1fc19d8c1cd6b39d
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-02-22 13:52:38 +0200

    Created reference to "Roll" button from "MainActivity"

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rollButton = findViewById(R.id.roll_button)
     }
 }
